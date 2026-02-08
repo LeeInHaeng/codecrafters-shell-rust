31a5e626edd8c8498dad36e7063a9cfa110b4205
LEE IN HAENG <lih0420@naver.com>
2026-02-08 23:56:46 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn command_execute(command: &str, command_args: &str) {
     }
 
     // execute command
-    match Command::new(check_command_executable_result.full_path).args(command_args.split_whitespace()).output() {
+    match Command::new(check_command_executable_result.command).args(command_args.split_whitespace()).output() {
         Ok(output) => {
             println!("{}", str::from_utf8(&output.stdout).unwrap());
         },
