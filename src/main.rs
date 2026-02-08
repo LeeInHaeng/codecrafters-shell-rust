e89c4f35e4084da4ed16a4d57d3eb4e39dcba5f7
LEE IN HAENG <lih0420@naver.com>
2026-02-08 19:30:04 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ fn main() {
 
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", command.trim());
+        command = command.trim().to_string();
+
+        if command == "exit" {
+            break;
+        }
+
+        println!("{}: command not found", command);
     }
 }
