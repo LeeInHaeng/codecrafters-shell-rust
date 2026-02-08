544bd280a12bc96f953a4ad99a9ad088a384f588
LEE IN HAENG <lih0420@naver.com>
2026-02-08 20:13:17 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const COMMAND: [&str; 3]= ["exit", "echo", "type"];
+
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
+
         io::stdin().read_line(&mut command).unwrap();
+        
         command = command.trim().to_string();
         let splited_command: Vec<&str> = command.split(' ').collect();
-        let command = splited_command[0].to_string();
+        let command = splited_command[0];
 
-        if command == "exit" {
-            break;
-        }
-
-        if command == "echo" {
-            command_echo( &splited_command[1..].join(" "));
+        if false == COMMAND.contains(&splited_command[1]) {
+            println!("{}: not found", splited_command[1]);
             continue;
         }
 
-        println!("{}: command not found", command);
+        match command {
+            "exit" => break,
+            "echo" => command_echo( &splited_command[1..].join(" ")),
+            "type" => command_type(splited_command[1]),
+            _ => println!("{}: command not found", command)
+        };
     }
 }
 
 fn command_echo(args: &str) {
     println!("{}", args);
+}
+
+fn command_type(args: &str) {
+    println!("{} is a shell builtin", args) ;
 }
