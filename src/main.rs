cf640e92ef6a5bee5660f7899370dc28fee8f57d
LEE IN HAENG <lih0420@naver.com>
2026-02-08 19:50:08 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,22 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim().to_string();
+        let splited_command: Vec<&str> = command.split(' ').collect();
+        let command = splited_command[0].to_string();
 
         if command == "exit" {
             break;
         }
 
+        if command == "echo" {
+            command_echo( &splited_command[1..].join(" "));
+            continue;
+        }
+
         println!("{}: command not found", command);
     }
+}
+
+fn command_echo(args: &str) {
+    println!("{}", args);
 }
